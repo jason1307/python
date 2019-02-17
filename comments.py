5810340d36597b145ce0a0dbfe941060bb988d48
jason1307 <45998580+jason1307@users.noreply.github.com>
2019-02-17 23:57:18 +0200

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,5 @@
+this is not a comment
+# this is a comment
+# this is also a comment
+this is not a comment
+# this is also a comment
---
+++ removecomments.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+f=open("comments.py","r",encoding="utf8")
+lines = f.readlines()
+f.close()
+
+f=open("comments.py","w",encoding="utf8")
+
+for line in lines:
+	if "#" in line:
+		l=line.strip()
+
+
+	else:
+		print (line)
+		f.write(line)
+
+f.close()
